43381342accc6ccada8e3294cccda6657a4e6ff4
Jatin Satani <jatinsatani111@gmail.com>
2023-12-07 10:36:17 +0530

    Done

---
+++ swapnowithtemp.py
@@ -0,0 +1,8 @@
+no1 = int(input("Enter number1 : "))
+no2 = int(input("Enter number2 : "))
+
+temp = no1
+no1 = no2
+no2 = temp    
+
+print("no1:", no1," no2:",no2)
---
+++ swapwitouttem.py
@@ -0,0 +1,8 @@
+no1 = int(input("Enter number1 : "))
+no2 = int(input("Enter number2 : "))
+
+no1 = no1 + no2       
+no2 = no1 - no2       
+no1 = no1 - no2       
+
+print("no1:", no1, "no2: ", no2)
