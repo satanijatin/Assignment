b6eeaf98f9efbcfe43a9a02eaf8fbc96037dcd77
Jatin Satani <jatinsatani111@gmail.com>
2023-12-18 16:51:40 +0530

    Done

---
+++ sumof5.py
@@ -0,0 +1,12 @@
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+
+if num1 == num2  or num1 + num2 == 5:
+    result = True
+else:
+    result = False
+
+
+print("Result:", result)
